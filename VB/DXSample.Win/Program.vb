7ed3b5a1e66d26cc44dc7fa6757bc61bc56fbee9
DevExpressExampleBot <fakeEmail>
2023-11-16 23:40:54 +0400

    Source auto update [skip ci]

--- VB/DXSample.Win/Program.vb
+++ VB/DXSample.Win/Program.vb
@@ -1,38 +1,34 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Configuration
 Imports System.Windows.Forms
-
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Security
-Imports DevExpress.ExpressApp.Win
-Imports DevExpress.Persistent.Base
-Imports DevExpress.Persistent.BaseImpl
 
 Namespace DXSample.Win
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
-			Dim winApplication As New DXSampleWindowsFormsApplication()
-			If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
-				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
-			End If
-			Try
-				DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register()
-								winApplication.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString
-				winApplication.Setup()
-				winApplication.Start()
-			Catch e As Exception
-				winApplication.HandleException(e)
-			End Try
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+            Dim winApplication As DXSampleWindowsFormsApplication = New DXSampleWindowsFormsApplication()
+            If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
+                winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+            End If
+
+            Try
+                Xpo.InMemoryDataStoreProvider.Register()
+                winApplication.ConnectionString = Xpo.InMemoryDataStoreProvider.ConnectionString
+                winApplication.Setup()
+                winApplication.Start()
+            Catch e As Exception
+                winApplication.HandleException(e)
+            End Try
+        End Sub
+    End Module
 End Namespace

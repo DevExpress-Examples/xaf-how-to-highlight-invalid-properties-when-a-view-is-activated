fad4cfd0ce166c8b2c53b614e0b63bfc594bae7e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2024-09-03 15:29:54 +0400

    Create 24.2.1+ branch

--- CS/EFCore/ValidateHighlightEF/ValidateHighlightEF.Blazor.Server/Startup.cs
+++ CS/EFCore/ValidateHighlightEF/ValidateHighlightEF.Blazor.Server/Startup.cs
@@ -53,6 +53,7 @@ public class Startup {
                     options.UseSqlServer(connectionString);
                     options.UseChangeTrackingProxies();
                     options.UseObjectSpaceLinkProxies();
+                    options.UseXafServiceProviderContainer(serviceProvider);
                     options.UseLazyLoadingProxies();
                 })
                 .AddNonPersistent();
